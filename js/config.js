49ed34a9781a4801440e1ff2116e9872996f57f6
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2025-07-13 11:38:09 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_qxc0Ug87X', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5c783q1gott4kn7bubf06vtp4d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-1_gjMnKjwzW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3d7gc5bc6ihn2qva68i48j1ufg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
